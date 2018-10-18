73ea8684e5b5129d823b24935fb5019e9a405de4
Neeraj Bhartiya <neeraj.bhartiya@ericsson.com>
2018-10-18 12:09:44 +0530

    change

--- webapp-addresbbokproject/src/test/java/TestNG/addressbook.java
+++ webapp-addresbbokproject/src/test/java/TestNG/addressbook.java
@@ -14,16 +14,20 @@ public class addressbook {
 	public void launch() {
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		  driver = new ChromeDriver();
-		  driver.get("http://169.144.116.234:9090/spring-boot-web-thymeleaf-1.0/");
+		  driver.get("http://10.184.20.170:8080/maven-addressbook/");
 		  driver.manage().window().maximize();
-		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 	}
   @Test
   public void addressbook() {
 	  
-	  	driver.findElement(By.placeholder("Username")).sendKeys("vik");
-	  	driver.findElement(By.placeholder("Password")).sendKeys("Password@123");
-  	  	driver.findElement(By.xpath("//div[@class='form-control btn btn-primary']")).click();
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
+	  driver.findElement(By.id("gwt-uid-5")).sendKeys("neeraj");
+	  driver.findElement(By.id("gwt-uid-7")).sendKeys("bhartiya");
+	  driver.findElement(By.id("gwt-uid-9")).sendKeys("8888888888");
+	  driver.findElement(By.id("gwt-uid-11")).sendKeys("neerajbhartiya@gmail.com");
+	  driver.findElement(By.id("gwt-uid-13")).sendKeys("06/06/2018");
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
 	 	  	  
   }
  
